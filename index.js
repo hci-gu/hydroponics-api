83351b59d0dbe5145facb5f8ca083bff562cc2e7
Sebastian Andreasson <xandse@ml-200403-001.local>
2020-12-02 14:18:31 +0100

    add sequelize with Image saving

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const moment = require('moment')
 const bodyParser = require('body-parser')
 const AWS = require('aws-sdk')
 const app = express()
+const cors = require('cors')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 
-const { BUCKET_NAME, REGION, KEY, SECRET } = process.env
+const { BUCKET_NAME, REGION, KEY, SECRET, POSTGRES_DB } = process.env
+
+const sequelize = new Sequelize(POSTGRES_DB ? POSTGRES_DB : 'sqlite::memory:')
+
+class Image extends Model {}
+Image.init(
+  {
+    imageUrl: DataTypes.STRING,
+    imageTaken: DataTypes.DATE,
+    growthStart: DataTypes.DATE,
+    lightHours: DataTypes.INTEGER,
+    temperature: DataTypes.DOUBLE,
+    plant: DataTypes.STRING,
+  },
+  { sequelize, modelName: 'user' }
+)
+
+sequelize.sync()
 
 const s3 = new AWS.S3({
   accessKeyId: KEY,
@@ -15,12 +34,20 @@ const s3 = new AWS.S3({
   signatureVersion: 'v4',
 })
 
+app.use(cors())
 app.use(bodyParser.json({ limit: '10mb', extended: true }))
 app.get('/', (req, res) => res.send('hello world'))
 
+app.get('/images', async (req, res) => {
+  const images = await Image.findAll()
+
+  res.send(images)
+})
+
 app.post('/image', (req, res) => {
   console.log('received image!')
-  const fileName = `${moment().format('YYYY-MM-DDTHH:mm')}.png`
+  const now = moment().format('YYYY-MM-DDTHH:mm')
+  const fileName = `${now}.png`
   console.log(fileName)
   const params = {
     Bucket: BUCKET_NAME,
@@ -28,12 +55,20 @@ app.post('/image', (req, res) => {
     Body: Buffer.from(req.body.data, 'base64'),
     ContentEncoding: 'base64',
     ContentType: 'image/png',
+    ACL: 'public-read-write',
   }
-  s3.putObject(params, (err, data) => {
+  s3.putObject(params, async (err, data) => {
     console.log('response from s3', err, data)
+    await Image.create({
+      imageUrl: `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/${fileName}`,
+      imageTaken: new Date(now),
+      growthStart: new Date('2020-12-01'),
+      lightHours: 12,
+      temperature: 27.5,
+      plant: 'Sallad',
+    })
+    res.sendStatus(200)
   })
-
-  res.sendStatus(200)
 })
 
 app.listen(3000)
